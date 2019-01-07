39c71afee8d3733f7aa18e302ce57ace7524f147
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-07 17:03:27 -0500

    Update svm_pretrainer_v2.py

--- svm_pretrainer_v2.py
+++ svm_pretrainer_v2.py
@@ -62,7 +62,7 @@ class QPretrainer():
         self.y = self.ts[1:,self.num_f + signal]
         # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
         Cs = [0.001,0.01, 0.1, 1, 10, 100]
-        gammas = [0.01, 0.1, 1, 10, 100]
+        gammas = [0.01, 0.1, 1, 10, 100, 1000]
         param_grid = {'C': Cs, 'gamma' : gammas}
         grid_search = GridSearchCV(svm.SVR(kernel='rbf'), param_grid, cv=self.nfolds)
         grid_search.fit(self.x, self.y)
@@ -105,12 +105,13 @@ class QPretrainer():
 if __name__ == '__main__':
     pt = QPretrainer()
     pt.load_datasets()
-    for i in range(0,4):
-        print('Training model '+str(i))
-        params = pt.train_model(i)
-        print('best_params_' + str(i) + ' = ',params)
-        mse = pt.evaluate_validation(params,i)
-        print('mean_squared_error_' + str(i) + ' = ' + str(mse))
-        pt.export_model(i)
+    #for i in range(0,4):
+    i=1
+    print('Training model '+str(i))
+    params = pt.train_model(i)
+    print('best_params_' + str(i) + ' = ',params)
+    mse = pt.evaluate_validation(params,i)
+    print('mean_squared_error_' + str(i) + ' = ' + str(mse))
+    pt.export_model(i)
     
     

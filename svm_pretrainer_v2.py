f0b06ef819d75736977a67e175aa5d7d80aba9d4
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-10 01:37:41 -0500

    Update svm_pretrainer_v2.py

--- svm_pretrainer_v2.py
+++ svm_pretrainer_v2.py
@@ -66,9 +66,8 @@ class QPretrainer():
         #print("Training action (", signal, ") self.y = ", self.y)
         # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
         Cs = [ 2e-12, 2e-8, 2e-4, 2e0, 2e4]
-        epsilon = [2e-2, 2e-1, 2e1]
         gammas = [2e-20, 2e-10, 2e0, 2e10]
-        param_grid = {'C': Cs, 'gamma' : gammas, 'epsilon': epsilon}
+        param_grid = {'C': Cs, 'gamma' : gammas}
         grid_search = GridSearchCV(svm.SVR(), param_grid, cv=self.nfolds)
         grid_search.fit(self.x, self.y)
         return grid_search.best_params_
@@ -81,7 +80,7 @@ class QPretrainer():
         # TEST, remve 1 and replace by self.num_f
         self.y_v = self.vs[1:,self.num_f + signal]
         # create SVM model with RBF kernel with existing parameters
-        self.svr_rbf = svm.SVR(C=params["C"], gamma=params["gamma"], epsilon=params["epsilon"])
+        self.svr_rbf = svm.SVR(C=params["C"], gamma=params["gamma"])
         # Fit the SVM modelto the data and evaluate SVM model on validation x
         self.x = self.ts[1:,0:self.num_f]
         self.y = self.ts[1:,self.num_f + signal]

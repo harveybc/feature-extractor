63ab6106bdb95f6deaf9bdf08a0af84b557138e6
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-19 13:26:01 -0500

    Update svm_pretrainer_v2.py

--- svm_pretrainer_v2.py
+++ svm_pretrainer_v2.py
@@ -68,9 +68,9 @@ class QPretrainer():
         # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
         #Cs = [1e-4, 1e-3, 1e-2, 1e-1, 2e0, 2e1, 2e2, 2e4]
         #gammas = [2e-20, 2e-10, 2e0, 2e10]
-        epsilons = [1e-12, 1e-8, 1e-4, 1e-2, 1e-1]
-        Cs = [1, 1e2, 1e3, 2e3, 5e3, 1e4, 1e5, 1e6, 1e8]
-        gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
+        #epsilons = [1e-12, 1e-8, 1e-4, 1e-2, 1e-1]
+        #Cs = [1, 1e2, 1e3, 2e3, 5e3, 1e4, 1e5, 1e6, 1e8]
+        #gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
         param_grid = {'C': Cs, 'epsilon':epsilons}
         grid_search = GridSearchCV(svm.SVR(gamma="auto"),param_grid, cv=self.nfolds)
         grid_search.fit(self.x, self.y)

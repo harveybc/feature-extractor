56fcc514ab7bc3396d577826fd2e0c32a2b0cedf
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-20 02:20:45 -0500

    Update svm_pretrainer_v2.py

--- svm_pretrainer_v2.py
+++ svm_pretrainer_v2.py
@@ -69,7 +69,7 @@ class QPretrainer():
         # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
         #Cs = [1e-4, 1e-3, 1e-2, 1e-1, 2e0, 2e1, 2e2, 2e4]
         #gammas = [2e-20, 2e-10, 2e0, 2e10]
-        epsilons = [ 2e-4, 2e-3, 2e-2, 2e-1, 1e-1]
+        epsilons = [1e-2, 1e-1]
         Cs = [2e-5,2e-4,2e-3,2e-2, 2e-1,2e1,2e2,2e3,2e4]
         #gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
         param_grid = {'C': Cs, 'epsilon':epsilons}
@@ -112,7 +112,7 @@ class QPretrainer():
         plt.title('Signal ' + str(signal))
         plt.legend()
         fig.savefig('predict_' + str(signal) + '.png')
-        if signal==4:
+        if signal==3:
             plt.show()
         else:
             plt.show(block=False)

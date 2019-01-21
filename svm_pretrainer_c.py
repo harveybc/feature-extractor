758310b2c57fe4132c8bcee0f6d0b99a9dd21b59
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-21 16:41:43 -0500

    Update svm_pretrainer_c.py

--- svm_pretrainer_c.py
+++ svm_pretrainer_c.py
@@ -70,8 +70,8 @@ class QPretrainer():
         #Cs = [1e-4, 1e-3, 1e-2, 1e-1, 2e0, 2e1, 2e2, 2e4]
         #gammas = [2e-20, 2e-10, 2e0, 2e10]
         epsilons = [1e-2, 1e-1,2e-1,3e-1,5e-1,1,2,4]
-        #Cs = [2e-5,2e-4,2e-3,2e-2, 2e-1,2e1,2e2,2e3,2e4]
-        Cs = [1, 10,100,1000]
+        Cs = [2e-5,2e-4,2e-3,2e-2, 2e-1,2e1,2e2,2e3,2e4]
+        #Cs = [1, 10,100,1000]
         #gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
         param_grid = {'C': Cs, 'epsilon':epsilons}
         grid_search = GridSearchCV(svm.SVR(gamma="auto"),param_grid, cv=self.nfolds)
@@ -137,7 +137,7 @@ class QPretrainer():
         Cs = [1, 10,100,1000]
         gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
         param_grid = {'C': Cs}
-        grid_search = GridSearchCV(svm.SVC(kernel="linear", gamma="auto"),param_grid, cv=self.nfolds)
+        grid_search = GridSearchCV(svm.SVC(kernel="rbf", gamma="auto"),param_grid, cv=self.nfolds)
         grid_search.fit(self.x, self.y)
         return grid_search.best_params_
     
@@ -149,7 +149,7 @@ class QPretrainer():
         # TEST, remve 1 and replace by self.num_f
         self.y_v = self.vs[1:,self.num_f + signal].astype(int)
         # create SVM model with RBF kernel with existing parameters
-        self.svr_rbf = svm.SVC(kernel="linear", C=params["C"], gamma="auto")
+        self.svr_rbf = svm.SVC(kernel="rbf", C=params["C"], gamma="auto")
         # Fit the SVM modelto the data and evaluate SVM model on validation x
         self.x = self.ts[1:,0:self.num_f]
         self.y = self.ts[1:,self.num_f + signal]

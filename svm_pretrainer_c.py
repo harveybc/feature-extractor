fbb0b14c86b5f259a4b5c0c9be844a45fbebc394
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-21 01:59:44 -0500

    Update svm_pretrainer_c.py

--- svm_pretrainer_c.py
+++ svm_pretrainer_c.py
@@ -133,7 +133,7 @@ class QPretrainer():
         #Cs = [1e-4, 1e-3, 1e-2, 1e-1, 2e0, 2e1, 2e2, 2e4]
         #gammas = [2e-20, 2e-10, 2e0, 2e10]
         epsilons = [1e-2, 1e-1,2e-1,3e-1,5e-1,1,2,4]
-        Cs = [2e-5,2e-4,2e-3,2e-2,2e-1,1,2e1,2e2,2e3,2e4]
+        Cs = [2e-8,2e-5,2e-4,2e-3,2e-2,2e-1,1,2e1,2e2,2e3,2e4,2e6]
         #gammas = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0.2,0.5, 0.9]
         param_grid = {'C': Cs}
         grid_search = GridSearchCV(svm.SVC(kernel="linear", gamma="auto"),param_grid, cv=self.nfolds)
@@ -175,7 +175,7 @@ class QPretrainer():
         plt.title('Signal ' + str(signal))
         plt.legend()
         fig.savefig('predict_' + str(signal) + '.png')
-        if signal==15:
+        if signal==18:
             plt.show()
         else:
             plt.show(block=False)

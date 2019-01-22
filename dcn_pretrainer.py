6fa951f20378c4cf3166ec16dcf9d876cb4a35af
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-22 15:43:00 -0500

    Update dcn_pretrainer.py

--- dcn_pretrainer.py
+++ dcn_pretrainer.py
@@ -258,7 +258,7 @@ if __name__ == '__main__':
     pt = QPretrainer()
     pt.load_datasets()
     #for i in range(0,pt.num_s):
-    for i in range(17,18):
+    for i in range(10,19):
         print('Training model '+str(i))
         # verifies if the actions are for classification(the last 6 ones)
         if (i>=10):

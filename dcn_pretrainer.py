ee54d1727d69f78282a1f2f8ad85953ca6d4c6dc
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-23 15:16:15 -0500

    Update dcn_pretrainer.py

--- dcn_pretrainer.py
+++ dcn_pretrainer.py
@@ -51,7 +51,7 @@ class QPretrainer():
         # svm model
         self.svr_rbf = []
         # Best so far 0.0001 error = 0.106 en 200 epochs, 2nd best, 0.0002 en 400 epochs=0.104
-        # 0.002 (Adamax default) = 
+        # 0.002 (Adamax default) = 0.137
         self.learning_rate = 0.002 
         #prev:300
         self.epochs = 400
@@ -65,20 +65,20 @@ class QPretrainer():
         # 0.1,0.5,0.025: 0.137
         # 0.3,0.15,0.08: 0.134
         # 0.4,0.1,0.05: 0.126 con 0.4,0.2,0.1 =0.142
+        # 0.2,0.1,lr=0.002: 
         
-        
-        model.add(Dropout(0.4,input_shape=(self.num_features,self.window_size)))
+        model.add(Dropout(0.2,input_shape=(self.num_features,self.window_size)))
         model.add(Conv1D(512, 3))
         model.add(Activation('sigmoid'))
         #model.add(MaxPooling1D(pool_size=2, strides=2))
         # second set of CONV => RELU => POOL
         
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.1))
         # mejor config so far: D0.4-512,D0.2-64,d0.1-32,16d64 error_vs=0.1 con 400 epochs y lr=0.0002
         model.add(Conv1D(64, 3))
         model.add(Activation('sigmoid'))
         
-        model.add(Dropout(0.1))
+        #model.add(Dropout(0.1))
         model.add(Conv1D(32, 3))
         model.add(Activation('sigmoid'))
         

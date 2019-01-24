4e2b1fb01a240cb5312e354e9e9d304915d579e1
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-24 17:53:13 -0500

    Update dcn_pretrainer.py

--- dcn_pretrainer.py
+++ dcn_pretrainer.py
@@ -61,7 +61,7 @@ class QPretrainer():
         # 0.002 (Adamax default) = 0.137
         self.learning_rate = 0.0002 
         #prev:400 0.11 
-        self.epochs = 600
+        self.epochs = 200
 
     def set_dcn_model(self):
 
@@ -232,7 +232,7 @@ class QPretrainer():
         self.svr_rbf = self.set_dcn_model()
         # train DCN model with the training data
         #best res so far: batch_size = 100   epochs=self.epochs
-        #con batch size=64, epochs=200, lr=0.0002 daba 0.25, cada epoca tardaba 6s
+        #con batch size=64, epochs=200, lr=0.0002 daba:, cada epoca tardaba:
         self.svr_rbf.fit(self.x, self.y, batch_size=64, epochs=self.epochs, verbose=1)
         return self.svr_rbf 
 

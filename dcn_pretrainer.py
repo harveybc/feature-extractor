fb30bb42a42555e727ca6a622a04af7ca5fef8b5
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-25 00:16:53 -0500

    Update dcn_pretrainer.py

--- dcn_pretrainer.py
+++ dcn_pretrainer.py
@@ -85,14 +85,16 @@ class QPretrainer():
         # con batch_norm: 0.204
         model.add(BatchNormalization())
         #con drop 0.1, e=0.299
-        #con drop 0.2, e=
+        #con drop 0.2, e=0.106
         model.add(Dropout(0.2))
         # mejor config so far: D0.4-512,D0.2-64,d0.1-32,16d64 error_vs=0.1 con 400 epochs y lr=0.0002
         model.add(Conv1D(64, 3))
         model.add(Activation('sigmoid'))
         #model.add(BatchNormalization())
         #model.add(BatchNormalization())
-        #model.add(Dropout(0.1))
+        #sin drop , e=0.106
+        #con drop 0.2, e=0.1
+        model.add(Dropout(0.2))
         model.add(Conv1D(32, 3))
         model.add(Activation('sigmoid'))
         #model.add(BatchNormalization())

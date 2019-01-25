0483b336120fc7323b37fe70a25627bdfe665c73
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-24 23:00:07 -0500

    Update dcn_pretrainer_cpu.py

--- dcn_pretrainer_cpu.py
+++ dcn_pretrainer_cpu.py
@@ -88,9 +88,9 @@ class QPretrainer():
         #model.add(MaxPooling1D(pool_size=2, strides=2))
         # second set of CONV => RELU => POOL
         
-        # con d=0.1 daba e=0.142
+        # con d=0.1 daba e=0.124
         # sin esta capa da:
-        model.add(Dropout(0.1))
+        #model.add(Dropout(0.1))
         
         # mejor config so far: D0.4-512,D0.2-64,d0.1-32,16d64 error_vs=0.1 con 400 epochs y lr=0.0002
         model.add(Conv1D(64, 3))

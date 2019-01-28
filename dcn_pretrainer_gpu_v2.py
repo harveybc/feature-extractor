5efdda8239cc272892d395e1348cdce1896627ea
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-01-28 04:14:21 -0500

    Update dcn_pretrainer_gpu_v2.py

--- dcn_pretrainer_gpu_v2.py
+++ dcn_pretrainer_gpu_v2.py
@@ -68,8 +68,9 @@ class QPretrainer():
         # con epochs 100, ave3 con 0.6 featureselect y batch size=1024  ave3= 0.305
         # con epochs 50, ave3 con 0.6 featureselect y batch size=1024  ave3= 0.264
         # con epochs 25, ave5 con 0.6 featureselect y batch size=1024  ave5= 0.39
-        # con epochs 25, lr=0.002 con 0.6 featureselect y batch size=1024  ave5= TODO
-        self.epochs = 25
+        # con epochs 25, lr=0.002 con 0.6 featureselect y batch size=1024  ave5= 0.335
+        # con epochs 50, lr=0.002 con 0.6 featureselect y batch size=1024  ave5= TODO
+        self.epochs = 50
         
         # number of validation tests to avarage during each training
         self.num_tests = 5

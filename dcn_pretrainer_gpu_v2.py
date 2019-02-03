ea8eff2d5f451a98154520953921ebeda899c27e
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-02-03 03:56:03 -0500

    Update dcn_pretrainer_gpu_v2.py

--- dcn_pretrainer_gpu_v2.py
+++ dcn_pretrainer_gpu_v2.py
@@ -253,7 +253,7 @@ class QPretrainer():
         #con batch size=512(64*8): , daba: loss=0.243 vs_e=0.251(0.241) cada epoca tardaba: 3s con 580us/step
         #con batch size=1024(128*8): , daba: loss=0.1787(0.251) vs_e=0.229 cada epoca tardaba: 3s con 540us/step
         #con batch size=2048(256*8): , daba: loss=0.27 vs_e=0.26 cada epoca tardaba: 3s con 540/step
-        self.svr_rbf.fit(self.x, self.y, batch_size=1024, epochs=self.epochs, verbose=1)
+        self.svr_rbf.fit(self.x, self.y, batch_size=1024, epochs=self.epochs, verbose=0)
         return self.svr_rbf 
 
         

644f9ffbb38c29d7062893cb9b4bc8dfd81412a7
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-02-04 20:45:39 -0500

    Update dcn_pretrainer_gpu_v2.py

--- dcn_pretrainer_gpu_v2.py
+++ dcn_pretrainer_gpu_v2.py
@@ -83,7 +83,7 @@ class QPretrainer():
         # mejor result 0.1 con dropout de 0.4 en 400 epochs con learning rate 0.0002 en config  521,64,32,16, en h4 2018 con indicator_period=70
         # 0.2,0.1,lr=0.0002 1200 eva: 0.117
         # 0.4,eva = 0.108 TODO respecto a 0.2
-        model.add(Dropout(0.2,input_shape=(self.num_features,self.window_size)))
+        model.add(Dropout(0.4,input_shape=(self.num_features,self.window_size)))
         model.add(Conv1D(512, 3))
         model.add(Activation('sigmoid'))
         # Sin batch_normalization daba: 0.204
@@ -92,7 +92,7 @@ class QPretrainer():
         # Con dropout = 0.1, e=0.168
         # con dropout = 0.2, e=0.121
         # con dropout = 0.4, e= 0.114
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.4))
         #sin capa de LSTM50,  e=0.107
         #con capa de LSTM50, e= 0.191
         #model.add(LSTM(units = 50, return_sequences = True))

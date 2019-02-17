b54e48410ac127b69026fe4c853840ffe55cce5d
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-02-17 15:22:30 -0500

    Update dcn_pretrainer_gpu_v2.py

--- dcn_pretrainer_gpu_v2.py
+++ dcn_pretrainer_gpu_v2.py
@@ -63,7 +63,7 @@ class QPretrainer():
         # 0.0005 = 0.142
         self.learning_rate = 0.0002
         
-        #epocsh 400, ava3 = 0.77
+        #epocsh 400, ava3 = 0.77, loss=0.4005
         #epocsh 1200, ava3 = TODO
         
         self.epochs = 1200

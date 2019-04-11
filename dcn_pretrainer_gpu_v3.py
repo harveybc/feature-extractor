527054d96287724851c16773655281de4eb2526f
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-04-11 00:24:21 -0500

    Update dcn_pretrainer_gpu_v3.py

--- dcn_pretrainer_gpu_v3.py
+++ dcn_pretrainer_gpu_v3.py
@@ -70,7 +70,7 @@ class QPretrainer():
         #epocsh 1200, ava3 = 0.66, loss=0.169
         
         
-        self.epochs = 1000
+        self.epochs = 10000
         # number of validation tests to avarage during each training
         self.num_tests = 3
 

d389bc25041d8e0c9ddf2f8e83e748765f01a5b4
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-04-23 23:58:30 -0500

    Update dcn_pretrainer_gpu_v3.py

--- dcn_pretrainer_gpu_v3.py
+++ dcn_pretrainer_gpu_v3.py
@@ -96,9 +96,8 @@ class QPretrainer():
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         
-        model.add(LSTM(units = 128, return_sequences = True, dropout = 0.4, recurrent_dropout = 0.4, input_shape=(self.num_features,self.window_size))) 
-        model.add(LSTM(units = 64, return_sequences = True, dropout = 0.4, recurrent_dropout = 0.4, input_shape=(self.num_features,self.window_size)))            
-        model.add(LSTM(units = 32, return_sequences = True, dropout = 0.4, recurrent_dropout = 0.4, input_shape=(self.num_features,self.window_size)))                        
+        model.add(LSTM(units = 256, return_sequences = True, dropout = 0.4, input_shape=(self.num_features,self.window_size))) 
+        model.add(LSTM(units = 64, return_sequences = True, dropout = 0.4,  input_shape=(self.num_features,self.window_size)))            
         model.add(LSTM(units=16, dropout = 0.4, recurrent_dropout = 0.4 ))
         
         model.add(Dense(32)) 

af87027266ee074c0d7f94af2df110aa681b8c54
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-05-08 23:52:12 -0500

    Update dcn_pretrainer_gpu_v3.py

--- dcn_pretrainer_gpu_v3.py
+++ dcn_pretrainer_gpu_v3.py
@@ -127,12 +127,12 @@ class QPretrainer():
         model = Sequential()
         # for observation[19][48], 19 vectors of 128-dimensional vectors,input_shape = (19, 48)
         # model.add(Dropout(0.6,input_shape=(self.num_features,self.window_size)))
-        model.add(Conv1D(128, 5, strides=2,use_bias=False, input_shape=(self.num_features,self.window_size)))
+        model.add(Conv1D(256, 5, strides=2,use_bias=False, input_shape=(self.num_features,self.window_size)))
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         
         model.add(Dropout(0.6))
-        model.add(Conv1D(340, 3, use_bias=False))
+        model.add(Conv1D(128, 3, use_bias=False))
         model.add(BatchNormalization())
         model.add(Activation('relu'))
        

b391b7d9d3249ef3cb705111aa252918049f85d2
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-05-29 18:50:07 -0500

    Update dcn_pretrainer_gpu_v3.py

--- dcn_pretrainer_gpu_v3.py
+++ dcn_pretrainer_gpu_v3.py
@@ -73,82 +73,26 @@ class QPretrainer():
         # number of validation tests to avarage during each training
         self.num_tests = 1
 
-    def set_dcn_model_r_alex(self):
-        # Deep Convolutional Neural Network for Regression
-        model = Sequential()
-        # for observation[19][48], 19 vectors of 128-dimensional vectors,input_shape = (19, 48)
-        # model.add(Dropout(0.6,input_shape=(self.num_features,self.window_size)))
-        model.add(Conv1D(48, 5, strides=2,use_bias=False, input_shape=(self.num_features,self.window_size)))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-        
-        model.add(MaxPooling1D(pool_size=3, strides=2))
-        
-        model.add(Conv1D(128, 3, use_bias=False))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-       
-        model.add(MaxPooling1D(pool_size=3, strides=2))
-        
-        model.add(Conv1D(200, 3, stride=1, padding=1, use_bias=False))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-       
-        model.add(Conv1D(200, 3, stride=1, padding=1, use_bias=False))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-       
-        model.add(Conv1D(128, 3, stride=1, padding=1, use_bias=False))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-       
-        model.add(MaxPooling1D(pool_size=3, strides=2))
-        
-        model.add(Dense(640)) 
-        model.add(Dropout(0.2))
-        model.add(Dense(640)) 
-        model.add(Dropout(0.2))
-        model.add(Dense(256)) 
-        model.add(Dropout(0.2))
-        #model.add(Dense(32)) 
-        #model.add(Dropout(0.4))
-        model.add(Dense(1, activation = 'linear')) 
-
-        # use SGD optimizer
-        opt = Adamax(lr=self.learning_rate)
-
-        #paralell_model = multi_gpu_model(model, gpus=2)
-        paralell_model = model
-
-        model.compile(loss="mae", optimizer=opt, metrics=["mse"])
-        return paralell_model 
-
     def set_dcn_model_r(self):
         # Deep Convolutional Neural Network for Regression
         model = Sequential()
         # for observation[19][48], 19 vectors of 128-dimensional vectors,input_shape = (19, 48)
-
         model.add(Conv1D(512, 5, strides=2,use_bias=False, input_shape=(self.num_features,self.window_size))) 
         model.add(BatchNormalization())  
-        model.add(Activation('relu'))
-        
+        model.add(Activation('relu'))        
         model.add(Conv1D(256, 3, use_bias=False)) 
         model.add(BatchNormalization())  
-        model.add(Activation('relu'))
-        
+        model.add(Activation('relu'))        
         #model.add(Dropout(0.6))
         #model.add(Conv1D(8, 3, use_bias=False))
         #model.add(BatchNormalization())
-        #model.add(Activation('relu'))
-        
+        #model.add(Activation('relu'))        
         model.add(LSTM(units = 256, input_shape=(self.num_features,self.window_size))) 
         model.add(BatchNormalization()) 
-
         #model.add(LSTM(units = 32, return_sequences = True, dropout = 0.4,  input_shape=(self.num_features,self.window_size)))            
         #model.add(LSTM(units = 16, return_sequences = True, dropout = 0.4, input_shape=(self.num_features,self.window_size)))                        
         #model.add(LSTM(units=32, dropout = 0.4, recurrent_dropout = 0.6 ))
-        #model.add(BatchNormalization())
- 
+        #model.add(BatchNormalization()) 
         model.add(Dense(640)) 
         model.add(BatchNormalization())
         model.add(Activation('hard_sigmoid'))
@@ -158,89 +102,11 @@ class QPretrainer():
         model.add(Activation('hard_sigmoid'))
         #model.add(Dropout(0.2))
         model.add(Dense(1, activation = 'linear')) 
-
         # use SGD optimizer
         opt = Adamax(lr=self.learning_rate)
-
         #paralell_model = multi_gpu_model(model, gpus=2)
         paralell_model = model 
-
         model.compile(loss="mse", optimizer=opt, metrics=["mae"])
-        return paralell_model 
-
-    def set_dcn_model_c(self):
-
-        # Deep Convolutional Neural Network for Regression
-        model = Sequential()
-        # for observation[19][48], 19 vectors of 128-dimensional vectors,input_shape = (19, 48)
-        # first set of CONV => RELU => POOL
-        # mejor result 0.1 con dropout de 0.4 en 400 epochs con learning rate 0.0002 en config  521,64,32,16, en h4 2018 con indicator_period=70
-        # 0.2,0.1,lr=0.0002 1200 eva: 0.117
-        # 0.4,eva = 0.108
-        model.add(Dropout(0.2,input_shape=(self.num_features,self.window_size)))
-        model.add(Conv1D(512, 3))
-        model.add(Activation('sigmoid'))
-        # Sin batch_normalization daba: 0.204
-        # Con batch normalization: e=0.168
-        model.add(BatchNormalization())
-        # Con dropout = 0.1, e=0.168
-        # con dropout = 0.2, e=0.121
-        # con dropout = 0.4, e= 0.114
-        model.add(Dropout(0.2))
-        #sin capa de LSTM50,  e=0.107
-        #con capa de LSTM50, e= 0.191
-        #model.add(LSTM(units = 50, return_sequences = True))
-        
-        #model.add(Dropout(0.2))
-        # mejor config so far: D0.4-512,D0.2-64,d0.1-32,16d64 error_vs=0.1 con 400 epochs y lr=0.0002
-        # sin capa de 64, eva = 0.114
-        # on capa de 128, eva = 0.125
-        # on capa de 32,  eva = 0.107
-        # on capa de 16,  eva = 0.114
-        model.add(Conv1D(32, 3))
-        model.add(Activation('sigmoid'))
-        #model.add(BatchNormalization())
-
-        # con otra capa de 32, eva5 = 0.126
-        # sin otra capa de 32, eva5 = 0.107, sin minmax normalization
-        # sin otra capa de 32, eva5 = 0.124 , con minmax normalization antes de power transform
-        #model.add(Conv1D(32, 3))
-        #model.add(Activation('sigmoid'))
-        #model.add(BatchNormalization())
-        #model.add(Dropout(0.1))
-        
-        # con capa de 16 da   eva5= 107
-        model.add(Conv1D(16, 3))
-        model.add(Activation('sigmoid'))
-        model.add(BatchNormalization())
-
-        #sin capa de LSTM50, eva3=0.104 probar con 400 epochs
-        #con capa de LSTM50, eva3= 0.212
-        #model.add(LSTM(units = 50, return_sequences = True))
-        
-        #model.add(MaxPooling1D(pool_size=2, strides=2))
-        # second set of CONV => RELU => POOL
-       # model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-       # con d=0.1 daba 0.11 con loss=0.08
-       # con d=0.2 daba 0.22 con loss=0.06
-        model.add(Dense(64, activation='sigmoid', kernel_initializer='glorot_uniform')) # valor óptimo:64 @400k
-       # model.add(Activation ('sigmoid'))
-        #model.add(BatchNormalization())
-
-        # output layer
-        model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-        model.add(Dense(1, activation = 'sigmoid'))
-        # multi-GPU support
-        #model = to_multi_gpu(model)
-        #self.reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.3, patience=5, min_lr=1e-4)
-        # use SGD optimizer
-        opt = Adamax(lr=self.learning_rate)
-        #opt = SGD(lr=self.learning_rate, momentum=0.9)
-        #paralell_model = multi_gpu_model(model, gpus=2)
-        paralell_model = model
-        paralell_model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-        #model.compile(loss="binary_crossentropy", optimizer="adamax", metrics=["accuracy"])
-        #model.compile(loss="mse", optimizer=opt, metrics=["accuracy"])
         return paralell_model 
 
     ## Load  training and validation datasets, initialize number of features and training signals
@@ -258,7 +124,6 @@ class QPretrainer():
         #ts_n = np.array(self.ts)
         #y_t = ts_n[0:,self.num_f + 0]         
         #print("y_t = ", y_t[0:30] )
-        
         self.vs_s = self.ts_g[(3*self.num_ticks)//4 : self.num_ticks,:]
         self.vs = self.vs_s.copy() 
         
@@ -290,17 +155,13 @@ class QPretrainer():
         #converts to nparray
         # TODO: Usando dataset completo ts_g en lugar de solo ts,incluyendo validation set, se hace separación por parámetro validation_split de fit
         self.ts = np.array(self.ts_g)
-        self.x_pre = self.ts[0:,0:self.num_f]
-        
-        #TODO: BBORRAR hasta print
+        self.x_pre = self.ts[0:, 0:self.num_f]
+        # TODO: BBORRAR hasta print
         #print("self.x_pre[0:30, self.num_f-1] = ", self.x_pre[0:30, self.num_f-1])
-        
         self.x = self.dcn_input(self.x_pre)
         self.y = self.ts[0:,self.num_f + signal]         
-        
         #print("signal = ",signal,"   self.y = ", self.y)         
         # TODO: Cambiar var svr_rbf por p_model
-        
         # setup the DCN model
         self.svr_rbf = self.set_dcn_model_r()
         # train DCN model with the training data
@@ -330,9 +191,7 @@ class QPretrainer():
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
         fig.savefig('predict_' + str(signal) + '_loss.png', dpi=600)
-        
         return self.svr_rbf 
-
     
     ## Evaluate the trained models in the validation set to obtain the error
     def evaluate_validation(self, params, signal):
@@ -348,15 +207,16 @@ class QPretrainer():
         # predict the class of in the validation set
         y_rbf = self.svr_rbf.predict(self.x_v)
         # TODO: test, quitar cuando x_v sea igual a obs de agend_dcn
-        with open('output_obs.csv' , 'w', newline='') as myfile:
-            wr = csv.writer(myfile)
-            wr.writerows(self.x_v)
+        print("self.x_v.shape = ", self.x_v.shape)
+        np.savetxt("",self.x_v,delimiter=",")
+        #with open('output_obs.csv' , 'w', newline='') as myfile:
+        #    wr = csv.writer(myfile)
+        #    wr.writerows(self.x_v)
         print("Finished generating validation set observations.")
         with open('output_act.csv' , 'w', newline='') as myfile:
             wr = csv.writer(myfile)
             wr.writerows(y_rbf)
         print("Finished generating validation set actions per observation.") 
-        
         #if signal == 0:
         #    print("Validation set y_rbf = ",y_rbf)
         # plot original and predicted data of the validation dataset
@@ -373,64 +233,10 @@ class QPretrainer():
         plt.legend()
         fig.savefig('predict_' + str(signal) + '.png', dpi=1000)
         return mean_squared_error(self.y_v, y_rbf)
-    
- 
- 
- # TODO: COREGIR RANGOS DE TS Y VS PARA TRAIN_MODEL_C Y EVALUATE_C
-  ## Returns best parameters
-    def train_model_c(self, signal):
-        #converts to nparray
-        self.ts = np.array(self.ts)
-        self.x_pre = self.ts[1:,0:self.num_f]
-        self.x = self.dcn_input(self.x_pre)
-        self.y = self.ts[1:,self.num_f + signal].astype(int)                  
-        # TODO: Cambiar var svr_rbf por p_model
-        # setup the DCN model
-        self.svr_rbf = self.set_dcn_model_c()
-        # train DCN model with the training data 
-        # con batch size =  128 dio 0.17
-        # con batch_size = 1024, ev=0.75(0.1)
-        # con batch_size = 512, ev = 0.176
-        self.svr_rbf.fit(self.x, self.y, batch_size=1024, epochs=self.epochs, verbose=1)
-        return self.svr_rbf 
-        
-    ## Evaluate the trained models in the validation set to obtain the error
-    def evaluate_validation_c(self, model, signal):
-        self.vs = np.array(self.vs)
-        self.x_v_pre = self.vs[1:,0:self.num_f]
-        self.x_v = self.dcn_input(self.x_v_pre)
-        # TEST, remve 1 and replace by self.num_f
-        self.y_v = self.vs[1:,self.num_f + signal].astype(int)
-        #if signal == 0:
-        #    print("Validation set self.x_v = ",self.x_v)
-        # predict the class of in the validation set
-        y_rbf = self.svr_rbf.predict_classes(self.x_v)
-        #if signal == 0:
-        #    print("Validation set y_rbf = ",y_rbf)
-        # plot original and predicted data of the validation dataset
-        lw = 2
-        # TODO: NO ES TS SINO VS
-        x_seq = list(range(0, self.vs.shape[0]-1))
-        # 0 = Buy/CloseSell/nopCloseBuy
-        #rint("x_seq.len = ", len(x_seq) , "y.len = " ,len(self.y_v) )
-        #fig=plt.figure()
-        #plt.plot(x_seq, self.y_v, color='darkorange', label='data')
-        #plt.plot(x_seq, y_rbf, color='navy', lw=lw, label='RBF model')
-        #plt.xlabel('data')
-        #plt.ylabel('target')
-        #plt.title('Signal ' + str(signal))
-        #plt.legend()
-        #fig.savefig('predict_cpu_' + str(signal) + '.png')
-        #if signal==18:
-        #    plt.show()
-        #else:
-        #    plt.show(block=False)
-        return accuracy_score(self.y_v, y_rbf)
  
     ## Export the trained models and the predicted validation set predictions, print statistics 
     def export_model(self, signal):
         self.svr_rbf.save(self.model_prefix + str(signal)+'.dcn') 
-
         
 # main function 
 if __name__ == '__main__':

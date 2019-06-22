ee5c39bb383a35d548447105a1dc5e3e4d6dd17a
Harvey Bastidas <harveybc@users.noreply.github.com>
2019-06-22 17:47:31 -0500

    Update dcn_pretrainer_gpu_v4.py

--- dcn_pretrainer_gpu_v4.py
+++ dcn_pretrainer_gpu_v4.py
@@ -77,10 +77,10 @@ class QPretrainer():
         # 0.0002 = 0.127
         # 0.0005 = 0.142
         # mejor leaning rate sin batch normalization + 0.0002
-        self.learning_rate = 0.00005 
+        self.learning_rate = 0.005 
         #epocsh 400, ava3 = TODO
         #epocsh 1200, ava3 = 0.66, loss=0.169
-        self.epochs = 30 
+        self.epochs = 100 
         # number of validation tests to avarage during each training
         self.num_tests = 1
 

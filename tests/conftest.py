866195cbc105b79a5cf62c0cbe35099f1063fddb
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-06-24 17:43:52 -0500

    updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from flaskr.db import get_db
 from flaskr.db import init_db
 
 # read in SQL for populating test data
-with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
+with open(os.path.join(os.path.dirname(__file__), "data/visualizer_test_data.sql"), "rb") as f:
     _data_sql = f.read().decode("utf8")
 
 

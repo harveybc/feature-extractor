1fed15a236291c13f0f00ccbdfbe4c5e1dfbcd8d
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-06-30 18:50:10 -0500

    Update __init__.py

--- feature_extractor/visualizer/__init__.py
+++ feature_extractor/visualizer/__init__.py
@@ -6,12 +6,12 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
         # store the database in the instance folder
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__)),
-        DATABASE=os.path.join(BASE_DIR, "test.sqlite")
+        DATABASE=os.path.join(app.BASE_DIR, "test.sqlite"),
     )
 
     if test_config is None:

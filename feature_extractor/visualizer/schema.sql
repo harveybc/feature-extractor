57d0bf8fa2e2f9a23825e0393d42e58808fb07d7
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-05 20:39:39 -0500

    Update schema.sql

--- feature_extractor/visualizer/schema.sql
+++ feature_extractor/visualizer/schema.sql
@@ -1,10 +1,18 @@
 -- Initialize the database.
 -- Drop any existing data and create empty tables.
 
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS processes;
 DROP TABLE IF EXISTS training_progress;
 DROP TABLE IF EXISTS validation_plots;
 DROP TABLE IF EXISTS validation_stats;
+
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
 
 CREATE TABLE "processes" (
 	"id"	INTEGER,

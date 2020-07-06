615c755819e5d964c4749d8c2b09fed91e0a31b7
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-05 21:26:53 -0500

    updated visualizer web if

--- feature_extractor/visualizer/__init__.py
+++ feature_extractor/visualizer/__init__.py
@@ -37,10 +37,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    from feature_extractor.visualizer import auth, blog
+    from feature_extractor.visualizer import auth, visualizer
 
     app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    app.register_blueprint(visualizer.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with

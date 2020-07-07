56196dff24ee362c691de8b84798e65fd37bf2f4
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-06 22:50:30 -0500

    updated visualizer

--- feature_extractor/visualizer/__init__.py
+++ feature_extractor/visualizer/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",

1708e0683c57231eccfe259942b5ef038ffc9463
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-06 20:12:32 -0500

    Update blog.py

--- feature_extractor/visualizer/blog.py
+++ feature_extractor/visualizer/blog.py
@@ -7,8 +7,8 @@ from flask import request
 from flask import url_for
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from visualizer.auth import login_required
+from visualizer.db import get_db
 
 bp = Blueprint("blog", __name__)
 

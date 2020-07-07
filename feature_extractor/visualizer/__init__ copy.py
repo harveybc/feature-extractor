c9bcf9db7cd400c6da62ada9b3576936f5d34f6f
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-06 19:06:49 -0500

    used the flask examples again

--- feature_extractor/visualizer/__init__ copy.py
+++ feature_extractor/visualizer/__init__ copy.py
@@ -6,11 +6,12 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        DATABASE=os.path.join(BASE_DIR, "test.sqlite"),
     )
 
     if test_config is None:
@@ -31,15 +32,15 @@ def create_app(test_config=None):
         return "Hello, World!"
 
     # register the database commands
-    from flaskr import db
+    from feature_extractor.visualizer import db
 
     db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog
+    from feature_extractor.visualizer import auth, visualizer
 
     app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    app.register_blueprint(visualizer.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with

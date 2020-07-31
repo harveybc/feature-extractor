8dd1024ae0f1e82e024be08b203d416af1ebf392
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-31 01:01:14 -0500

    updated output_plugin

--- feature_extractor/visualizer/__init__.py
+++ feature_extractor/visualizer/__init__.py
@@ -55,26 +55,18 @@ def create_app(test_config=None):
 
     db.init_app(app)
 
-    with app.app_context():
-        # TODO: replace 0 in vis_data by process_id, obtained as the first process_id belonging to the current user.  
-        # read the data to be visualized using the using the Feature extractor instance, preinitialized in __init__.py with input and output plugins entry points.
-        vis_data = app.config['FE'].ep_input.load_data(app.config['P_CONFIG'], 0)
-        # use the output plugin entry point to get the path of the template for the visualizer blueprint
-        plugin_folder = app.config['FE'].ep_output.template_path(app.config['P_CONFIG'])
 
-    # construct the visualizer blueprint using the plugin folder as template folder
-    bp = Blueprint("visualizer", __name__,  template_folder=plugin_folder)
-    
     # apply the blueprints to the app
     from feature_extractor.visualizer import auth, visualizer
-
-
-
-
-
-
+    
+    # get the output plugin template folder
+    plugin_folder = fe.ep_output.template_path(p_config)
+    # construct the blueprint 
+    vis_bp = visualizer.visualizer_blueprint(plugin_folder)
+    
+    # register the blueprints
     app.register_blueprint(auth.bp)
-    app.register_blueprint(visualizer.bp) 
+    app.register_blueprint(vis_bp) 
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
--- feature_extractor/visualizer/visualizer.py
+++ feature_extractor/visualizer/visualizer.py
@@ -14,116 +14,116 @@ from werkzeug.exceptions import abort
 from feature_extractor.visualizer.auth import login_required
 from feature_extractor.visualizer.db import get_db
 from flask import current_app
-from . import app
-
-with app.app_context():
-    # read the data to be visualized using the using the Feature extractor instance, preinitialized in __init__.py with input and output plugins entry points.
-    vis_data = current_app.config['FE'].ep_input.load_data(current_app.config['P_CONFIG'], 0)
-    # use the output plugin entry point to get the path of the template for the visualizer blueprint
-    plugin_folder = current_app.config['FE'].ep_output.template_path(current_app.config['P_CONFIG'])
-
-# construct the visualizer blueprint using the plugin folder as template folder
-bp = Blueprint("visualizer", __name__,  template_folder=plugin_folder)
-# TODO: replace 0 in vis_data by process_id, obtained as the first process_id belonging to the current user.    
-
-@bp.route("/")
-@login_required
-def index():
-    return render_template("\\templates\\visualizer\\index.html", p_config = current_app.config['P_CONFIG'])
 
 
-def get_post(id, check_author=True):
-    """Get a post and its author by id.
+def visualizer_blueprint(plugin_folder):
 
-    Checks that the id exists and optionally that the current user is
-    the author.
+    # construct the visualizer blueprint using the plugin folder as template folder
+    bp = Blueprint("visualizer", __name__,  template_folder=plugin_folder)
+    
+    @bp.route("/")
+    @login_required
+    def index():
+        # read the data to be visualized using the using the Feature extractor instance, preinitialized in __init__.py with input and output plugins entry points.
+        # TODO: replace 0 in vis_data by process_id, obtained as the first process_id belonging to the current user.    
+        vis_data = current_app.config['FE'].ep_input.load_data(current_app.config['P_CONFIG'], 0)
+        return render_template("\\templates\\visualizer\\index.html", p_config = current_app.config['P_CONFIG'], vis_data =  vis_data)
 
-    :param id: id of post to get
-    :param check_author: require the current user to be the author
-    :return: the post with author information
-    :raise 404: if a post with the given id doesn't exist
-    :raise 403: if the current user isn't the author
-    """
-    results = (
-        get_db()
-        .execute(
-            "SELECT p.id, title, body, created, author_id, username"
-            " FROM post p JOIN user u ON p.author_id = u.id"
-            " WHERE p.id = ?",
-            (id,),
+
+    def get_post(id, check_author=True):
+        """Get a post and its author by id.
+
+        Checks that the id exists and optionally that the current user is
+        the author.
+
+        :param id: id of post to get
+        :param check_author: require the current user to be the author
+        :return: the post with author information
+        :raise 404: if a post with the given id doesn't exist
+        :raise 403: if the current user isn't the author
+        """
+        results = (
+            get_db()
+            .execute(
+                "SELECT p.id, title, body, created, author_id, username"
+                " FROM post p JOIN user u ON p.author_id = u.id"
+                " WHERE p.id = ?",
+                (id,),
+            )
+            .fetchone()
         )
-        .fetchone()
-    )
-    # verify if the query returned no results
-    if results is None:
-        abort(404, "Post id {id} doesn't exist.")
-        
-    return results
+        # verify if the query returned no results
+        if results is None:
+            abort(404, "Post id {id} doesn't exist.")
+            
+        return results
 
 
-@bp.route("/create", methods=("GET", "POST"))
-@login_required
-def create():
-    """Create a new post for the current user."""
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
+    @bp.route("/create", methods=("GET", "POST"))
+    @login_required
+    def create():
+        """Create a new post for the current user."""
+        if request.method == "POST":
+            title = request.form["title"]
+            body = request.form["body"]
+            error = None
 
-        if not title:
-            error = "Title is required."
+            if not title:
+                error = "Title is required."
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
-            )
-            db.commit()
-            return redirect(url_for("visualizer.index"))
+            if error is not None:
+                flash(error)
+            else:
+                db = get_db()
+                db.execute(
+                    "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                    (title, body, g.user["id"]),
+                )
+                db.commit()
+                return redirect(url_for("visualizer.index"))
 
-    return render_template("visualizer/create.html")
-
-
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
-@login_required
-def update(id):
-    """Update a post if the current user is the author."""
-    post = get_post(id)
-
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
-
-        if not title:
-            error = "Title is required."
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for("visualizer.index"))
-
-    return render_template("visualizer/update.html", post=post)
+        return render_template("visualizer/create.html")
 
 
-@bp.route("/<int:id>/delete", methods=("POST",))
-@login_required
-def delete(id):
-    """Delete a post.
+    @bp.route("/<int:id>/update", methods=("GET", "POST"))
+    @login_required
+    def update(id):
+        """Update a post if the current user is the author."""
+        post = get_post(id)
 
-    Ensures that the post exists and that the logged in user is the
-    author of the post.
-    """
-    get_post(id)
-    db = get_db()
-    db.execute("DELETE FROM post WHERE id = ?", (id,))
-    db.commit()
-    return redirect(url_for("visualizer.index"))
+        if request.method == "POST":
+            title = request.form["title"]
+            body = request.form["body"]
+            error = None
+
+            if not title:
+                error = "Title is required."
+
+            if error is not None:
+                flash(error)
+            else:
+                db = get_db()
+                db.execute(
+                    "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
+                )
+                db.commit()
+                return redirect(url_for("visualizer.index"))
+
+        return render_template("visualizer/update.html", post=post)
+
+
+    @bp.route("/<int:id>/delete", methods=("POST",))
+    @login_required
+    def delete(id):
+        """Delete a post.
+
+        Ensures that the post exists and that the logged in user is the
+        author of the post.
+        """
+        get_post(id)
+        db = get_db()
+        db.execute("DELETE FROM post WHERE id = ?", (id,))
+        db.commit()
+        return redirect(url_for("visualizer.index"))
+    
+    return bp

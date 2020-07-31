3297a21001880fe31f0cf9c76fc55f27b0930e7e
Harvey Bastidas <harveybc@users.noreply.github.com>
2020-07-31 00:22:09 -0500

    Update __init__.py

--- feature_extractor/visualizer/__init__.py
+++ feature_extractor/visualizer/__init__.py
@@ -57,12 +57,12 @@ def create_app(test_config=None):
     
     # TODO: replace 0 in vis_data by process_id, obtained as the first process_id belonging to the current user.  
     # read the data to be visualized using the using the Feature extractor instance, preinitialized in __init__.py with input and output plugins entry points.
-    vis_data = current_app.config['FE'].ep_input.load_data(current_app.config['P_CONFIG'], 0)
+    vis_data = app.config['FE'].ep_input.load_data(app.config['P_CONFIG'], 0)
     # use the output plugin entry point to get the path of the template for the visualizer blueprint
-    plugin_folder = current_app.config['FE'].ep_output.template_path(current_app.config['P_CONFIG'])
+    plugin_folder = app.config['FE'].ep_output.template_path(app.config['P_CONFIG'])
 
     # construct the visualizer blueprint using the plugin folder as template folder
-    bp = Blueprint("visualizer", __name__,  template_folder=current_app.config['FE'].ep_output.template_path(current_app.config['P_CONFIG']))
+    bp = Blueprint("visualizer", __name__,  template_folder=app.config['FE'].ep_output.template_path(app.config['P_CONFIG']))
     
     # apply the blueprints to the app
     from feature_extractor.visualizer import auth, visualizer

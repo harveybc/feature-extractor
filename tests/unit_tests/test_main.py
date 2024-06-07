06638e5f30d87fea6bfefde828d89dcd19908ebf
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-07 02:25:10 -0500

    Update test_main.py

--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -5,6 +5,7 @@ from unittest.mock import patch, MagicMock
 from app.main import main
 import json
 
+
 @patch("app.cli.parse_args")
 @patch("app.config_handler.load_config")
 @patch("app.config_handler.save_config")

608d9dd73f77c80c33fb71dbe94add036afc5531
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-09 01:37:37 -0500

    update

--- app/cli.py
+++ app/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import config
 
 def parse_args():
     """
@@ -13,8 +14,8 @@ def parse_args():
     parser.add_argument('csv_file', type=str, help='Path to the CSV file to process.')
 
     # Optional arguments for saving and loading model parameters
-    parser.add_argument('-se', '--save_encoder', type=str, help='Filename to save the trained encoder model.')
-    parser.add_argument('-sd', '--save_decoder', type=str, help='Filename to save the trained decoder model.')
+    parser.add_argument('-se', '--save_encoder', type=str, default=config.SAVE_ENCODER_PATH, help='Filename to save the trained encoder model.')
+    parser.add_argument('-sd', '--save_decoder', type=str, default=config.SAVE_DECODER_PATH, help='Filename to save the trained decoder model.')
        
     # Optional arguments for loading and loading model parameters
     parser.add_argument('-le', '--load_encoder', type=str, help='Filename to load encoder parameters from.')
@@ -25,27 +26,28 @@ def parse_args():
     parser.add_argument('-ed', '--evaluate_decoder', type=str, help='Filename for outputting decoder evaluation results.')
 
     # Optional arguments for selecting plugins
-    parser.add_argument('-ep', '--encoder_plugin', type=str, default='default_encoder', help='Name of the encoder plugin to use.')
-    parser.add_argument('-dp', '--decoder_plugin', type=str, default='default_decoder', help='Name of the decoder plugin to use.')
+    parser.add_argument('-ep', '--encoder_plugin', type=str, default=config.DEFAULT_ENCODER_PLUGIN, help='Name of the encoder plugin to use.')
+    parser.add_argument('-dp', '--decoder_plugin', type=str, default=config.DEFAULT_DECODER_PLUGIN, help='Name of the decoder plugin to use.')
 
     # Optional argument for specifying the sliding window size
-    parser.add_argument('-ws', '--window_size', type=int, default=512, help='Sliding window size to use for processing time series data.')
+    parser.add_argument('-ws', '--window_size', type=int, default=config.WINDOW_SIZE, help='Sliding window size to use for processing time series data.')
     
     # Optional arguments related to autoencoder configuration and training
-    parser.add_argument('-me', '--max_error', type=float, help='Maximum MSE error to stop the training process.')
-    parser.add_argument('-is', '--initial_size', type=int, help='Initial size of the encoder/decoder interface.')
-    parser.add_argument('-ss', '--step_size', type=int, help='Step size to reduce the size of the encoder/decoder interface on each iteration.')
+    parser.add_argument('-me', '--max_error', type=float, default=config.MAXIMUM_MSE_THRESHOLD, help='Maximum MSE error to stop the training process.')
+    parser.add_argument('-is', '--initial_size', type=int, default=config.INITIAL_ENCODING_DIM, help='Initial size of the encoder/decoder interface.')
+    parser.add_argument('-ss', '--step_size', type=int, default=config.ENCODING_STEP_SIZE, help='Step size to reduce the size of the encoder/decoder interface on each iteration.')
 
     # Optional argument for remote logging, monitoring and storage of trained models
-    parser.add_argument('-rl', '--remote_log', type=str, help='URL of a remote data-logger API endpoint.')
+    parser.add_argument('-rl', '--remote_log', type=str, default=config.REMOTE_LOG_URL, help='URL of a remote data-logger API endpoint.')
 
     # Optional argument for downloading and executing a remote JSON configuration
-    parser.add_argument('-rc', '--remote_config', type=str, help='URL of a remote JSON configuration file to download and execute.')
+    parser.add_argument('-rc', '--remote_config', type=str, default=config.REMOTE_CONFIG_URL, help='URL of a remote JSON configuration file to download and execute.')
+
+    # Optional argument for loading local config file
+    parser.add_argument('-lc', '--load_config', type=str, help='Path to a local JSON configuration file to load and execute.')
 
     # Optional argument for quiet mode
-    parser.add_argument('-qm', '--quiet_mode', type=int, help='Do not show results on console.')
-
-
+    parser.add_argument('-qm', '--quiet_mode', type=bool, default=config.DEFAULT_QUIET_MODE, help='Do not show results on console.')
 
     args, unknown = parser.parse_known_args()
     return args, unknown
--- app/config.py
+++ app/config.py
@@ -2,32 +2,38 @@
 
 # Path configurations
 CSV_INPUT_PATH = './csv_input.csv'  # Default path for CSV input if not specified
-MODEL_SAVE_PATH = './model_out'          # Default directory to save trained models
-MODEL_LOAD_PATH = './model_in'          # Default directory to load models from
 CSV_OUTPUT_PATH = './csv_output.csv'  # Default path for CSV output if not specified
 CONFIG_SAVE_PATH = './config_out.json'  # Default file to save preprocessing configurations
 CONFIG_LOAD_PATH = './config_in.json'  # Default file to load configurations from
 
 # Model and training configurations
-DEFAULT_ENCODER_PLUGIN = 'basic_encoder'         # Default encoder plugin name
-DEFAULT_DECODER_PLUGIN = 'basic_decoder'         # Default decoder plugin name
-TRAINING_BATCH_SIZE = 32                         # Default batch size for model training
-EPOCHS = 10                                      # Default number of epochs for training
+DEFAULT_ENCODER_PLUGIN = 'default'  # Default encoder plugin name
+DEFAULT_DECODER_PLUGIN = 'default'  # Default decoder plugin name
+TRAINING_BATCH_SIZE = 32  # Default batch size for model training
+EPOCHS = 10  # Default number of epochs for training
 
 # Plugin configurations
 PLUGIN_DIRECTORY = 'app/plugins/'  # Directory containing all plugins
 
 # Remote logging and configuration
-REMOTE_LOG_URL = 'http://remote-log-server/api/logs'  # Default URL for remote logging
-REMOTE_CONFIG_URL = 'http://remote-config-server/api/config'  # Default URL for remote configuration
+REMOTE_LOG_URL = None  # Default URL for remote logging
+REMOTE_CONFIG_URL = None  # Default URL for remote configuration
 
 # Sliding window size for time series processing
-WINDOW_SIZE = 512                                # Default window size for sliding window technique
+WINDOW_SIZE = 512  # Default window size for sliding window technique
 
 # Autoencoder configuration
-INITIAL_ENCODING_DIM = 256                       # Initial encoding dimension for the encoder
-ENCODING_STEP_SIZE = 32                          # Step size to reduce the encoding dimension
-MAXIMUM_MSE_THRESHOLD = 0.01                     # Maximum MSE threshold for stopping training
+INITIAL_ENCODING_DIM = 256  # Initial encoding dimension for the encoder
+ENCODING_STEP_SIZE = 32  # Step size to reduce the encoding dimension
+MAXIMUM_MSE_THRESHOLD = 0.3  # Maximum MSE threshold for stopping training
 
 # Quiet mode
 DEFAULT_QUIET_MODE = False  # Default setting for quiet mode
+
+# Default remote credentials
+REMOTE_USERNAME = 'test'
+REMOTE_PASSWORD = 'pass'
+
+# Save paths for encoder and decoder
+SAVE_ENCODER_PATH = './encoder_ann.keras'  # Default path to save the encoder model
+SAVE_DECODER_PATH = './decoder_ann.keras'  # Default path to save the decoder model

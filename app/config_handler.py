1a2bf02458a98005ad12d5dbadf3f5da26d44907
Harvey Bastidas <harveybc@ingeni-us.com>
2024-06-09 02:19:01 -0500

    Update config_handler.py

--- app/config_handler.py
+++ app/config_handler.py
@@ -1,30 +1,44 @@
 import json
 import requests
+from config import (DEFAULT_ENCODER_PLUGIN, DEFAULT_DECODER_PLUGIN, 
+                    CSV_OUTPUT_PATH, REMOTE_LOG_URL, REMOTE_CONFIG_URL, 
+                    REMOTE_USERNAME, REMOTE_PASSWORD, DEFAULT_QUIET_MODE,
+                    MAXIMUM_MSE_THRESHOLD, INITIAL_ENCODING_DIM, 
+                    ENCODING_STEP_SIZE, SAVE_ENCODER_PATH, SAVE_DECODER_PATH, 
+                    WINDOW_SIZE, CONFIG_SAVE_PATH, CONFIG_LOAD_PATH)
 
 # Define default values for the configuration
 DEFAULT_VALUES = {
-    'encoder_plugin': 'default_encoder',
-    'decoder_plugin': 'default_decoder',
-    'output_file': 'output.csv',
-    'remote_log': 'http://localhost:60500/preprocessor/feature_extractor/create',
-    'remote_save_config': 'http://localhost:60500/preprocessor/feature_extractor/create',
-    'remote_load_config': 'http://localhost:60500/preprocessor/feature_extractor/detail/1',
-    'remote_username': 'test',
-    'remote_password': 'pass',
-    'quiet_mode': False,
-    'max_error': 0.01,
-    'initial_size': 256,
-    'step_size': 32,
+    'encoder_plugin': DEFAULT_ENCODER_PLUGIN,
+    'decoder_plugin': DEFAULT_DECODER_PLUGIN,
+    'output_file': CSV_OUTPUT_PATH,
+    'remote_log': REMOTE_LOG_URL,
+    'remote_save_config': REMOTE_CONFIG_URL,
+    'remote_load_config': REMOTE_CONFIG_URL,
+    'remote_username': REMOTE_USERNAME,
+    'remote_password': REMOTE_PASSWORD,
+    'quiet_mode': DEFAULT_QUIET_MODE,
+    'max_error': MAXIMUM_MSE_THRESHOLD,
+    'initial_size': INITIAL_ENCODING_DIM,
+    'step_size': ENCODING_STEP_SIZE,
     'csv_file': '',
-    'headers': True,
-    'window_size': 10
+    'headers': False,
+    'window_size': WINDOW_SIZE,
+    'save_encoder': SAVE_ENCODER_PATH,
+    'save_decoder': SAVE_DECODER_PATH,
+    'load_encoder': None,
+    'load_decoder': None,
+    'evaluate_encoder': None,
+    'evaluate_decoder': None,
+    'remote_config': None,
+    'load_config': CONFIG_LOAD_PATH
 }
 
 def load_config(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
-def save_config(config, path='config_out.json'):
+def save_config(config, path=CONFIG_SAVE_PATH):
     config_to_save = {k: v for k, v in config.items() if k not in DEFAULT_VALUES or config[k] != DEFAULT_VALUES[k]}
     with open(path, 'w') as f:
         json.dump(config_to_save, f, indent=4)

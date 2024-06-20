2c7bc849c0a0e460db94b24703535e2218c29477
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-20 02:04:11 -0500

    Update test_data_processing.py

--- tests/integration_tests/test_data_processing.py
+++ tests/integration_tests/test_data_processing.py
@@ -1,9 +1,9 @@
 import pytest
 import numpy as np
 import pandas as pd
+from unittest.mock import MagicMock, patch, call
 from app.data_processor import train_autoencoder, process_data
 from app.autoencoder_manager import AutoencoderManager
-from unittest.mock import MagicMock
 
 @pytest.fixture
 def config():
@@ -32,23 +32,23 @@ def autoencoder_manager():
     return mock_autoencoder_manager
 
 def test_train_autoencoder(autoencoder_manager, mock_data, config):
-    trained_manager = train_autoencoder(autoencoder_manager, mock_data.values, config['mse_threshold'], config['initial_encoding_dim'], config['encoding_step_size'], config['incremental_search'], config['epochs'])
-    assert trained_manager is not None
-    autoencoder_manager.build_autoencoder.assert_called()
-    autoencoder_manager.train_autoencoder.assert_called()
+    with patch('app.data_processor.AutoencoderManager', return_value=autoencoder_manager):
+        trained_manager = train_autoencoder(autoencoder_manager, mock_data.values, config['mse_threshold'], config['initial_encoding_dim'], config['encoding_step_size'], config['incremental_search'], config['epochs'])
+        assert trained_manager is not None
+        autoencoder_manager.build_autoencoder.assert_called()
+        autoencoder_manager.train_autoencoder.assert_called()
 
 def test_process_data(config, mock_data):
     # Mock the load_csv function to return mock_data
     from app.data_handler import load_csv
     load_csv_mock = MagicMock(return_value=mock_data)
-    pd.read_csv = load_csv_mock
-
-    processed_data, debug_info = process_data(config)
-    assert processed_data is not None
-    assert isinstance(processed_data, dict)
-    assert len(processed_data) == mock_data.shape[1]
-    for column, data in processed_data.items():
-        assert data.shape[1] == config['window_size']  # Check if windowed data has correct shape
+    with patch('pandas.read_csv', load_csv_mock):
+        processed_data, debug_info = process_data(config)
+        assert processed_data is not None
+        assert isinstance(processed_data, dict)
+        assert len(processed_data) == mock_data.shape[1]
+        for column, data in processed_data.items():
+            assert data.shape[1] == config['window_size']  # Check if windowed data has correct shape
 
 if __name__ == "__main__":
     pytest.main()

fab1068891741bb2d6f09176e269b65c2cc79500
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-20 01:20:58 -0500

    Update test_plugin_loader.py

--- tests/unit_tests/test_plugin_loader.py
+++ tests/unit_tests/test_plugin_loader.py
@@ -2,7 +2,7 @@ import pytest
 from app.plugin_loader import load_plugin, load_encoder_decoder_plugins, get_plugin_params
 
 def test_load_plugin_success():
-    plugin_class, required_params = load_plugin('feature_extractor.encoders', 'encoder_plugin_ann')
+    plugin_class, required_params = load_plugin('feature_extractor.encoders', 'default')
     assert plugin_class.plugin_params == {'epochs': 10, 'batch_size': 256}
     assert required_params == ['epochs', 'batch_size']
 
@@ -16,12 +16,12 @@ def test_load_plugin_general_exception():
     assert 'Failed to find plugin' in str(excinfo.value)
 
 def test_load_encoder_decoder_plugins():
-    encoder_plugin, encoder_params, decoder_plugin, decoder_params = load_encoder_decoder_plugins('encoder_plugin_ann', 'decoder_plugin_ann')
+    encoder_plugin, encoder_params, decoder_plugin, decoder_params = load_encoder_decoder_plugins('default', 'default')
     assert encoder_plugin.plugin_params == {'epochs': 10, 'batch_size': 256}
     assert decoder_plugin.plugin_params == {'param2': 'value2'}
 
 def test_get_plugin_params_success():
-    params = get_plugin_params('feature_extractor.encoders', 'encoder_plugin_ann')
+    params = get_plugin_params('feature_extractor.encoders', 'default')
     assert params == {'epochs': 10, 'batch_size': 256}
 
 def test_get_plugin_params_key_error():

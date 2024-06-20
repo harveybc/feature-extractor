ce2a9a2b3be2207451a1196d854fed6618cf13ff
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-20 01:16:15 -0500

    Update test_plugin_loader.py

--- tests/unit_tests/test_plugin_loader.py
+++ tests/unit_tests/test_plugin_loader.py
@@ -1,23 +1,24 @@
 import pytest
 from app.plugin_loader import load_plugin, load_encoder_decoder_plugins, get_plugin_params
-from app.plugins.encoder_plugin_ann import Plugin as EncoderPlugin
 
 def test_load_plugin_success():
     plugin_class, required_params = load_plugin('feature_extractor.encoders', 'encoder_plugin_ann')
     assert plugin_class.plugin_params == {'epochs': 10, 'batch_size': 256}
     assert required_params == ['epochs', 'batch_size']
 
+def test_load_plugin_key_error():
+    with pytest.raises(ImportError):
+        load_plugin('feature_extractor.encoders', 'non_existent_plugin')
+
 def test_load_plugin_general_exception():
     with pytest.raises(Exception) as excinfo:
         load_plugin('feature_extractor.encoders', 'non_existent_plugin')
-    assert 'Plugin non_existent_plugin not found in group feature_extractor.encoders' in str(excinfo.value)
+    assert 'Failed to find plugin' in str(excinfo.value)
 
 def test_load_encoder_decoder_plugins():
     encoder_plugin, encoder_params, decoder_plugin, decoder_params = load_encoder_decoder_plugins('encoder_plugin_ann', 'decoder_plugin_ann')
     assert encoder_plugin.plugin_params == {'epochs': 10, 'batch_size': 256}
-    assert encoder_params == ['epochs', 'batch_size']
     assert decoder_plugin.plugin_params == {'param2': 'value2'}
-    assert decoder_params == ['param2']
 
 def test_get_plugin_params_success():
     params = get_plugin_params('feature_extractor.encoders', 'encoder_plugin_ann')
@@ -30,7 +31,4 @@ def test_get_plugin_params_key_error():
 def test_get_plugin_params_general_exception():
     with pytest.raises(Exception) as excinfo:
         get_plugin_params('feature_extractor.encoders', 'non_existent_plugin')
-    assert 'Plugin non_existent_plugin not found in group feature_extractor.encoders' in str(excinfo.value)
-
-if __name__ == '__main__':
-    pytest.main()
+    assert 'Failed to find plugin' in str(excinfo.value)

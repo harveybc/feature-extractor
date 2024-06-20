6578400b63b598318e82031409e452ec931e04b3
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-20 00:16:38 -0500

    Update test_main.py

--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -33,12 +33,15 @@ from app.config import DEFAULT_VALUES
     'incremental_search': True
 })
 def test_main_with_invalid_arguments(mock_process_data, mock_save_config, mock_load_config, mock_parse_args):
-    mock_parse_args.return_value = (MagicMock(), [])
+    mock_args = MagicMock()
+    mock_parse_args.return_value = (mock_args, ['--invalid_argument'])
     mock_process_data.return_value = (MagicMock(), {})
     mock_load_config.return_value = {}
 
-    mock_args = ['script_name', 'tests/data/csv_sel_unb_norm_512.csv', '--save_encoder', './encoder_ann.keras', '--save_decoder', './decoder_ann.keras']
-    with patch('sys.argv', mock_args + ['--invalid_argument']):
-        with patch('sys.stderr', new_callable=MagicMock()) as mock_stderr:
-            with pytest.raises(SystemExit):  # Expect SystemExit due to sys.exit(1)
-                main()
+    mock_args.csv_input_path = 'tests/data/csv_sel_unb_norm_512.csv'
+    mock_args.save_encoder = './encoder_ann.keras'
+    mock_args.save_decoder = './decoder_ann.keras'
+
+    with patch('sys.argv', ['script_name', 'tests/data/csv_sel_unb_norm_512.csv', '--save_encoder', './encoder_ann.keras', '--save_decoder', './encoder_ann.keras', '--invalid_argument']):
+        with pytest.raises(SystemExit):  # Expect SystemExit due to sys.exit(1)
+            main()

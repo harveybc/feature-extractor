9a744b4b0d747efabfb724ab837d8f3b2bf0b28d
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-20 00:39:39 -0500

    Update test_plugin_loader.py

--- tests/unit_tests/test_plugin_loader.py
+++ tests/unit_tests/test_plugin_loader.py
@@ -11,13 +11,6 @@ def test_load_plugin_success():
         plugin_class, required_params = load_plugin('feature_extractor.encoders', 'mock_plugin')
         assert plugin_class.plugin_params == {'param1': 'value1'}
         assert required_params == ['param1']
-        mock_entry_point.load.assert_called_once()
-
-def test_load_plugin_key_error():
-    with patch('importlib.metadata.entry_points', return_value={'feature_extractor.encoders': []}):
-        with pytest.raises(ImportError) as excinfo:
-            load_plugin('feature_extractor.encoders', 'nonexistent_plugin')
-        assert 'Plugin nonexistent_plugin not found in group feature_extractor.encoders.' in str(excinfo.value)
 
 def test_load_plugin_general_exception():
     with patch('importlib.metadata.entry_points', side_effect=Exception('General error')):
@@ -40,8 +33,6 @@ def test_load_encoder_decoder_plugins():
         assert encoder_params == ['param1']
         assert decoder_plugin.plugin_params == {'param2': 'value2'}
         assert decoder_params == ['param2']
-        mock_encoder_entry_point.load.assert_called_once()
-        mock_decoder_entry_point.load.assert_called_once()
 
 def test_get_plugin_params_success():
     mock_entry_point = EntryPoint(name='mock_plugin', value='mock.module:Plugin', group='feature_extractor.encoders')
@@ -50,11 +41,10 @@ def test_get_plugin_params_success():
     with patch('importlib.metadata.entry_points', return_value={'feature_extractor.encoders': [mock_entry_point]}):
         params = get_plugin_params('feature_extractor.encoders', 'mock_plugin')
         assert params == {'param1': 'value1'}
-        mock_entry_point.load.assert_called_once()
 
 def test_get_plugin_params_key_error():
     with patch('importlib.metadata.entry_points', return_value={'feature_extractor.encoders': []}):
-        params = get_plugin_params('feature_extractor.encoders', 'nonexistent_plugin')
+        params = get_plugin_params('feature_extractor.encoders', 'mock_plugin')
         assert params == {}
 
 def test_get_plugin_params_general_exception():

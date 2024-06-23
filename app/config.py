8956c945e7b8ad42297556cb66c276aa2f605f84
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-23 14:40:07 -0500

    update

--- app/config.py
+++ app/config.py
@@ -2,27 +2,24 @@
 
 # Default configuration values
 DEFAULT_VALUES = {
-    'csv_input_path': './csv_input.csv',
-    'csv_output_path': './csv_output.csv',
-    'config_save_path': './config_out.json',
-    'config_load_path': './config_in.json',
-    'encoder_plugin': 'default',
-    'decoder_plugin': 'default',
-    'training_batch_size': 128,
-    'epochs': 10,
-    'plugin_directory': 'app/plugins/',
-    'remote_log_url': None,
-    'remote_config_url': None,
-    'window_size': 512,
-    'initial_encoding_dim': 8,
-    'encoding_step_size': 4,
-    'mse_threshold': 0.005,
-    'quiet_mode': False,
-    'remote_username': 'test',
-    'remote_password': 'pass',
-    'save_encoder_path': './encoder_ann.keras',
-    'save_decoder_path': './decoder_ann.keras',
-    'force_date': False,
-    'headers': False,
-    'incremental_search': True
+    'csv_file': './csv_input.csv',                   # Default path for input CSV
+    'save_encoder': './encoder_model.h5',            # Default filename to save the trained encoder model
+    'save_decoder': './decoder_model.h5',            # Default filename to save the trained decoder model
+    'load_encoder': None,                            # Default filename to load encoder parameters from
+    'load_decoder': None,                            # Default filename to load decoder parameters from
+    'evaluate_encoder': './encoder_eval.csv',        # Default filename for outputting encoder evaluation results
+    'evaluate_decoder': './decoder_eval.csv',        # Default filename for outputting decoder evaluation results
+    'encoder_plugin': 'default',                     # Default encoder plugin to use
+    'decoder_plugin': 'default',                     # Default decoder plugin to use
+    'window_size': 512,                              # Default sliding window size for processing time series data
+    'threshold_error': 0.005,                        # Default MSE error threshold to stop the training processes
+    'initial_size': 8,                               # Default initial size of the encoder/decoder interface
+    'step_size': 4,                                  # Default step size to adjust the size of the encoder/decoder interface
+    'remote_log': None,                              # Default URL of a remote data-logger API endpoint
+    'remote_config': None,                           # Default URL of a remote JSON configuration file to download and execute
+    'load_config': './config_in.json',               # Default path to load a configuration file
+    'save_config': './config_out.json',              # Default path to save the current configuration
+    'quiet_mode': False,                             # Default setting to suppress output messages
+    'force_date': False,                             # Default setting to include date in the output CSV files
+    'incremental_search': True                       # Default setting to enable incremental search for interface size
 }

1331b4734113ece957f8c74cb65bd2e7e08b62f9
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-22 21:55:21 -0500

    update

--- app/plugins/decoder_plugin_cnn.py
+++ app/plugins/decoder_plugin_cnn.py
@@ -54,15 +54,9 @@ class Plugin:
 
         self.model = Sequential(name="decoder")
 
-        # Adding Dense layers
+        # Adding a single Dense layer
         self.model.add(Dense(layer_sizes[0], input_shape=(interface_size,), activation='relu', name="decoder_input"))
         print(f"Added Dense layer with size: {layer_sizes[0]} as decoder_input")
-        if len(layer_sizes) > 1:
-            self.model.add(Dense(layer_sizes[1], activation='relu'))
-            print(f"Added Dense layer with size: {layer_sizes[1]}")
-        else:
-            self.model.add(Dense(output_shape, activation='relu'))
-            print(f"Added Dense layer with size: {output_shape}")
 
 
         self.model.add(Reshape((layer_sizes[1], 1)))

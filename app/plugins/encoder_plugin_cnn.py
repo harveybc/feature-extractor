44a760a5f498956263b31ef3a70bfe27238901c6
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-22 23:16:28 -0500

    Update encoder_plugin_cnn.py

--- app/plugins/encoder_plugin_cnn.py
+++ app/plugins/encoder_plugin_cnn.py
@@ -70,7 +70,8 @@ class Plugin:
                 kernel_size = 7
             # add the conv and maxpooling layers
             x = Conv1D(filters=size, kernel_size=kernel_size, activation='relu', padding='same')(x)
-            x = MaxPooling1D(pool_size=pool_size)(x)
+            if layers_index < (len(layers)-1):
+                x = MaxPooling1D(pool_size=pool_size)(x)
 
         x = Flatten()(x)
         #x = Dense(layers[len(layers)-1], activation='relu')(x)

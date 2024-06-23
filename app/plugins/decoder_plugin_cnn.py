62fb87590c472177988c1e6a4659bff6ab3582fc
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-22 23:52:47 -0500

    Update decoder_plugin_cnn.py

--- app/plugins/decoder_plugin_cnn.py
+++ app/plugins/decoder_plugin_cnn.py
@@ -78,8 +78,8 @@ class Plugin:
             if i < (len(layer_sizes) - 1):
                 next_size = layer_sizes[i + 1]
                 upsample_factor = next_size // reshape_size
-                #print(f"Added UpSampling1D layer with upsample factor: {upsample_factor}")
-                #self.model.add(UpSampling1D(size=upsample_factor))
+                print(f"Added UpSampling1D layer with upsample factor: {upsample_factor}")
+                self.model.add(UpSampling1D(size=upsample_factor))
             else:
                 next_size = output_shape
             

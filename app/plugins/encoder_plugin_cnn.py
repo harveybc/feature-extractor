5513cdf26f867a68a626f3cb857a363bf9afaf38
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-23 03:56:08 -0500

    Update encoder_plugin_cnn.py

--- app/plugins/encoder_plugin_cnn.py
+++ app/plugins/encoder_plugin_cnn.py
@@ -72,7 +72,7 @@ class Plugin:
             if size > 512:
                 kernel_size = 7
             # add the conv and maxpooling layers
-            x = Conv1D(filters=size, kernel_size=5, activation='relu', padding='same')(x)
+            x = Conv1D(filters=size, kernel_size=kernel_size, activation='relu', padding='same')(x)
             x = MaxPooling1D(pool_size=pool_size)(x)
             
         x = Flatten()(x)

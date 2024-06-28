3bd3e241a0981ef9c32418dc69c9021c29fc1374
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-06-28 07:52:56 -0500

    Update config_handler.py

--- app/config_handler.py
+++ app/config_handler.py
@@ -3,6 +3,8 @@
 import json
 import requests
 from app.config import DEFAULT_VALUES
+from app.plugins.encoder_plugin_cnn import Plugin as EncoderPluginCNN
+from app.plugins.decoder_plugin_transformer import Plugin as DecoderPluginTransformer
 
 def load_config(file_path):
     with open(file_path, 'r') as f:
@@ -10,9 +12,18 @@ def load_config(file_path):
     return config
 
 def save_config(config, path='config_out.json'):
+    # Retrieve plugin-specific default values
+    encoder_plugin_default_values = EncoderPluginCNN().plugin_params
+    decoder_plugin_default_values = DecoderPluginTransformer().plugin_params
+    
     config_to_save = {}
     for k, v in config.items():
         if k not in DEFAULT_VALUES or v != DEFAULT_VALUES[k]:
+            # Check for plugin-specific default values
+            if k in encoder_plugin_default_values and v == encoder_plugin_default_values[k]:
+                continue
+            if k in decoder_plugin_default_values and v == decoder_plugin_default_values[k]:
+                continue
             config_to_save[k] = v
     with open(path, 'w') as f:
         json.dump(config_to_save, f, indent=4)

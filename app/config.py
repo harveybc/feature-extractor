54e7c7f543a6850e27b08c3841ce3361e911b706
Harvey Bastidas <harveybc@ingeni-us.com>
2024-07-11 02:16:05 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ DEFAULT_VALUES = {
     'encoder_plugin': 'default',
     'decoder_plugin': 'default',
     'window_size': 128,
-    'threshold_error': 0.001,
+    'threshold_error': 0.003,
     'initial_size': 8,
     'step_size': 8,
     'save_log': './debug_out.json',

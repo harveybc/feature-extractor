3517cb83c141ccf53ba4eba8f573ba034c637dc2
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-04 21:05:34 -0500

    Update decoder_plugin_cnn.py

--- app/plugins/decoder_plugin_cnn.py
+++ app/plugins/decoder_plugin_cnn.py
@@ -59,7 +59,7 @@ class Plugin:
             if layer_sizes[i] > 512:
                 kernel_size = 7
             self.model.add(Conv1DTranspose(next_size, kernel_size=kernel_size, padding='same', activation='relu', kernel_initializer=HeNormal()))
-            self.model.add(Dropout(self.params['dropout_rate']))
+            # self.model.add(Dropout(self.params['dropout_rate'])) 
             reshape_size = layer_sizes[i]
             next_size = layer_sizes[i + 1]
             upsample_factor = next_size // reshape_size

226d74c39c7fe398caaae26c9fc9b94171337ff7
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-11 07:47:11 -0500

    Update decoder_plugin_cnn.py

--- app/plugins/decoder_plugin_cnn.py
+++ app/plugins/decoder_plugin_cnn.py
@@ -78,7 +78,7 @@ class Plugin:
         for size in layer_sizes[1:-1]:
             kernel_size = 3 if size <= 64 else 5 if size <= 512 else 7
             self.model.add(Conv1DTranspose(filters=size, kernel_size=kernel_size, padding='same', activation='relu', kernel_initializer=HeNormal(), kernel_regularizer=l2(0.01)))
-            print(f"After Conv1DTranspose (filters={size}): {self.model.layers[-1].output_shape}")
+            #print(f"After Conv1DTranspose (filters={size}): {self.model.layers[-1].output_shape}")
             self.model.add(BatchNormalization())
             print(f"After BatchNormalization: {self.model.layers[-1].output_shape}")
             self.model.add(Dropout(self.params['dropout_rate'] / 2))

6e6d32f496f45b3d608cf4c1dc800a88436b1787
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-13 14:18:50 -0500

    Update encoder_plugin_cnn.py

--- app/plugins/encoder_plugin_cnn.py
+++ app/plugins/encoder_plugin_cnn.py
@@ -94,7 +94,7 @@ class Plugin:
         # Add the last dense layer
         x = Dense(interface_size, input_shape=(interface_size,), activation=LeakyReLU(alpha=0.1), kernel_initializer=HeNormal(), kernel_regularizer=l2(0.001))(x)
         # Add the output reshape layer
-        output = Reshape((1, input_shape[1]))(x)
+        outputs = Reshape((1, input_shape))(x)
         # Build the encoder model
         self.encoder_model = Model(inputs=inputs, outputs=outputs, name="encoder")
                 # Define the Adam optimizer with custom parameters

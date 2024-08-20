aa4174104494ea3358faaa12e3628811995d56c3
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-19 21:27:07 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 DEFAULT_VALUES = {
-    'input_file': './tests/data/y_d1_target.csv',
-    'validation_file': './tests/data/y_d2_target.csv',
+    'input_file': './tests/data/y_d1_c.csv',
+    'validation_file': './tests/data/y_d2_c.csv',
     'output_file': './csv_output.csv',
     'save_encoder': './encoder_model.h5',
     'save_decoder': './decoder_model.h5',

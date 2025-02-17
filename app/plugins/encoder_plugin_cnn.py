68eb9446bae2bc0bfbfffdacbf44465c0b180ef1
Harvey Bastidas <harveybc@ingeni-us.com>
2025-02-17 02:33:34 -0500

    Update encoder_plugin_cnn.py

--- app/plugins/encoder_plugin_cnn.py
+++ app/plugins/encoder_plugin_cnn.py
@@ -97,7 +97,7 @@ class Plugin:
                    name="conv1d_final")(x)
         x = BatchNormalization(name="batch_norm_final")(x)
         # Global average pooling converts the 3D output to a 1D vector.
-        #x = GlobalAveragePooling1D()(x)
+        x = GlobalAveragePooling1D()(x)
         outputs = x
 
         self.encoder_model = Model(inputs=inputs, outputs=outputs, name="encoder_cnn")

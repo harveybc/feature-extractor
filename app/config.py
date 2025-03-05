083b9616c35969dbf56a2fe0e1982ac09c867fc2
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-04 20:49:23 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -29,7 +29,7 @@ DEFAULT_VALUES = {
     'headers': True,
     'epochs': 1000,  # Add epochs here
     'batch_size': 128,  # Add batch_size here
-    'learning_rate': 0.00001,  # Add learning_rate here
+    'learning_rate': 0.001,  # Add learning_rate here
     'dataset_periodicity': '1h',  # Add dataset_periodicity here, can be 1m, 5m, 15m, 30m, 1h, 4h, daily
     'max_steps':6300, # max number of rows to read from input file
     'use_mmd':True,

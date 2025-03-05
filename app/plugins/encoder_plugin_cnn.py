feeef2eb8f217ce918d90a6f9d029cc831aeb3d5
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-05 00:23:21 -0500

    update

--- app/plugins/encoder_plugin_cnn.py
+++ app/plugins/encoder_plugin_cnn.py
@@ -11,11 +11,11 @@ import tensorflow as tf
 class Plugin:
     plugin_params = {
         'batch_size': 128,
-        'intermediate_layers': 3,
+        'intermediate_layers': 10,
         'initial_layer_size': 128,
         'layer_size_divisor': 2,
         'learning_rate': 0.0001,
-        'l2_reg': 1e-3,
+        'l2_reg': 1e-4,
         'activation': 'tanh'
     }
     # For the encoder, "interface_size" is the latent dimension,

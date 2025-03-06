ad86e7dbb04d06bbc0c8012728247ab08907a514
Harvey Bastidas <harveybc@ingeni-us.com>
2025-03-06 11:21:59 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -40,7 +40,7 @@ DEFAULT_VALUES = {
     'mmd_sigma': 1.0,  # adjust as needed
     'statistical_loss_weight': 1.0,  # adjust as needed
     'use_pos_enc': False,
-    'l2_reg': 1e-5,
+    'l2_reg': 1e-6,
     'early_monitor': 'val_loss',
     'early_patience': 45,
     'include_base': True # includes high, low, close and open into the output csv when evaluating an encoder

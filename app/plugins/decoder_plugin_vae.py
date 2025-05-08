9c98a6bf746cdd4d6c85680940f54fba0b62af1e
Harvey Bastidas <harveybc@users.noreply.github.com>
2025-05-08 01:06:13 -0500

    update

--- app/plugins/decoder_plugin_vae.py
+++ app/plugins/decoder_plugin_vae.py
@@ -56,7 +56,7 @@ class Plugin:
 
         # --- Decoder input (latent) ---
         decoder_input = Input(
-            shape=(lstm_units,),  # Match the bottleneck layer size
+            shape=(num_filters,),  # Match the bottleneck layer size
             name="decoder_input"
         )
         x = decoder_input
